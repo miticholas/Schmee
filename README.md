88f1137def931b18f0096c73d6bb89fee302288a
b0mberb0b <ns_miticholas@yahoo.com>
2017-01-01 21:31:34 -0800

    1st commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Drupal 8 to power theater schmeater website. 
---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+var livereload = require('gulp-livereload')
+var uglify = require('gulp-uglifyjs');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+
+
+
+
+gulp.task('imagemin', function () {
+    return gulp.src('./themes/schmeater_theme/images/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('./themes/schmeater_theme/images'));
+});
+
+
+gulp.task('sass', function () {
+  gulp.src('./themes/schmeater_theme/sass/**/*.scss')
+    .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./themes/schmeater_theme/css'));
+});
+
+
+gulp.task('uglify', function() {
+  gulp.src('./themes/schmeater_theme/lib/*.js')
+    .pipe(uglify('main.js'))
+    .pipe(gulp.dest('./themes/schmeater_theme/js'))
+});
+
+gulp.task('watch', function(){
+    livereload.listen();
+
+    gulp.watch('./themes/schmeater_theme/sass/**/*.scss', ['sass']);
+    gulp.watch('./themes/schmeater_theme/lib/*.js', ['uglify']);
+    gulp.watch(['./themes/schmeater_theme/css/style.css', './themes/schmeater_theme/**/*.twig', './themes/schmeater_theme/js/*.js'], function (files){
+        livereload.changed(files)
+    });
+});
